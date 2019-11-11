107ec0f60497b54c273fd2720b787618f2a8f64f
Soeren SPROESSIG <soeren.sproessig@external.thalesgroup.com>
2019-11-11 14:34:19 +0100

    adds initial hello world Java project

---
+++ java/src/main/java/com.github.ssproessig.stackable_sax_parser/Application.java
@@ -0,0 +1,13 @@
+package com.github.ssproessig.stackable_sax_parser;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+
+@Slf4j
+class Application {
+
+  public static void main(String[] args) {
+    log.info("Hello, World");
+  }
+}
